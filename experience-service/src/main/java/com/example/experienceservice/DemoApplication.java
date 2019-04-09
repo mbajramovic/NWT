6a96a3bb5cba8a8e6cc6818adec924515d125671
alicamera <aalic6@etf.unsa.ba>
2019-04-10 00:44:43 +0200

    Eureka i sinhrona kom. (user-scholarship)

--- experience-service/src/main/java/com/example/experienceservice/DemoApplication.java
+++ experience-service/src/main/java/com/example/experienceservice/DemoApplication.java
@@ -3,7 +3,9 @@ package com.example.experienceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoApplication {
 
